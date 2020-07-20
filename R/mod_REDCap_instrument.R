1a180378fb6f0434a739b13c7035acb639ab1eab
David Mayer <david.mayer@cuanschutz.edu>
2020-07-20 15:33:15 -0600

    let's only scroll the instrument questions, why not? - keep the box header and instrument selector visible at all times.

--- R/mod_REDCap_instrument.R
+++ R/mod_REDCap_instrument.R
@@ -17,15 +17,16 @@ redcap_instrument_ui <- function(id) {
     useShinydashboard(),
     useShinyjs(),
     useSweetAlert(),
-    div(style='max-height:600px; overflow-y:scroll',
-      shinydashboard::box(title = "REDCap Instrument",
-                          width = '100%',
-                          status = 'danger',
-                          solidHeader = F,
-                          uiOutput(ns('instrument_select')),
-                          uiOutput(ns('instrument_ui')) %>% withSpinner(type = 5, color = '#e83a2f') 
-                          )
-      ),
+    
+    shinydashboard::box(title = "REDCap Instrument",
+                        width = '100%',
+                        status = 'danger',
+                        solidHeader = F,
+                        uiOutput(ns('instrument_select')),
+                        div(style='max-height:600px; overflow-y:scroll',
+                            uiOutput(ns('instrument_ui')) %>% withSpinner(type = 5, color = '#e83a2f') 
+                            )
+                        ),
     shinydashboard::box(title = 'Upload to REDCap',
                         width = '100%',
                         status = 'danger',
